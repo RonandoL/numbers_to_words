95b437b07259762e6418bd05efe3875f854d1a56
Ronando Long <ronando.long@gmail.com>
2015-09-14 15:02:44 -0700

    Spec failed, as expected, to translate integer in the thousands to word form.

--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -13,4 +13,8 @@ describe("Fixnum#numbers_to_words") do
   it("translates triple digit number into its written word form") do
     expect((173).numbers_to_words()).to(eq("one hundred and seventy three"))
   end
+
+  it("translates four digit number into its written word form") do
+    expect((9743).numbers_to_words()).to(eq("nine thousand seven hundred and forty three"))
+  end
 end
